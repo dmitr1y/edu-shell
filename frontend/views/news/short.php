20e5c6bde2ccd745483be80e3ad2aec018071550
Dmitriy Konstantinov <aser00707@ya.ru>
2017-10-13 16:06:31 +0300

    improve short news view

--- frontend/views/news/short.php
+++ frontend/views/news/short.php
@@ -1,4 +1,11 @@
-<h3> <a href="<?='/news/'.$model->slug?>"> <?= $model->header ?> </a></h3>
-<div class="meta">
-    <p>Автор: <b><?= $model->author ?></b> Дата публикации: <b><?= $model->date ?></p></b>
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        <h3 class="panel-title"><a href="<?= '/news/' . $model->slug ?>"> <?= $model->header ?> </a></h3>
+    </div>
+    <div class="panel-body">
+        short content
+    </div>
+    <div class="panel-footer">
+        <p>Автор: <b><?= $model->author ?></b> Дата публикации: <b><?= $model->date ?></p></b>
+    </div>
 </div>
